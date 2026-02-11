b41df98dec3993743e8f7afb4585790395d348e0
Harsh Sharma <sharma.harsh9313@gmail.com>
2026-02-11 12:26:42 +0530

    today's practise

---
+++ src/array/substract_product_and_sum_of_interger.java
@@ -0,0 +1,18 @@
+package array;
+
+public class substract_product_and_sum_of_interger {
+        public int subtractProductAndSum(int n) {
+            int product =1;
+            int sum=0;
+            int num=n;
+            while(num>0){
+                int temp= num%10;
+                product = product * temp;
+                sum = sum + temp;
+                num=num/10;
+            }
+            int result = product - sum;
+            return result;
+        }
+    }
+
