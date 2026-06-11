f9e2a12ae3a140dd88f7e5230d3b621a0b85f62f
Harsh Sharma <sharma.harsh9313@gmail.com>
2026-06-11 18:56:07 +0530

    today's practise nearly sorted array

--- src/Date_25_feb/nearly_sorted_array.java
+++ src/Date_25_feb/nearly_sorted_array.java
@@ -10,21 +10,22 @@ public class nearly_sorted_array {
             int mid = s + (e - s) / 2;
             if (arr[mid] == target) {
                 return mid;
-            } else if (mid-1>0 && arr[mid - 1] == target) {
+            } else if (mid-1>=0 && arr[mid - 1] == target) {
                 return mid - 1;
             } else if (mid+1<n && arr[mid + 1] == target) {
                 return mid + 1;
             } else if (arr[mid] < target) {
                 s = mid+2;
             } else {
-                e = mid+2;
+                e = mid-2;
             }
         }
         return -1;
     }
 
     public static void main(String[] args) {
-        int[] arr = {20, 10, 40, 30, 50, 70, 60};
-        System.out.println(nearly_sorted(arr, 60));
+//        int[] arr = {20, 10, 40, 30, 50, 70, 60};
+        int[] arr={1,3,2};
+        System.out.println(nearly_sorted(arr, 1));
     }
 }
